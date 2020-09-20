b4a8577d9c0d22127fbd916b8277031050887191
Eduardo Arias <contact.eduardoarias@gmail.com>
2020-09-20 04:24:17 -0300

    first commit, REST_JPA springboot example

---
+++ src/test/java/cl/ndk/RestJpa/RestJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ndk.RestJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
